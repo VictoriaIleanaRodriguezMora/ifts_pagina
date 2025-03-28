ed861bd9ec656e37be3c5aeb8640a3cc652acebb
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-03-27 21:55:35 -0300

    feat: Completo db json, creo rutas

---
+++ src/app/contacto/page.js
@@ -0,0 +1,8 @@
+export default function Contacto() {
+    return (
+        <div>
+            <h1>Contacto</h1>
+            <p>Envíanos un mensaje: contacto@ejemplo.com</p>
+        </div>
+    );
+}
---
+++ src/app/proyectos/page.js
@@ -0,0 +1,12 @@
+// app/proyectos/page.js
+export default function Proyectos() {
+    return (
+      <div>
+        <h1>Proyectos</h1>
+        <ul>
+          <li>Proyecto 1</li>
+          <li>Proyecto 2</li>
+        </ul>
+      </div>
+    );
+  }
