ebc73bac3b7a40013ba000f7907436408bab6bd3
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-07-24 09:13:44 -0300

    Fix: Logica y manejo de atributos para accesibilidad

--- src/app/header/header.jsx
+++ src/app/header/header.jsx
@@ -1,45 +1,73 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
+
 const Header = () => {
     const pathname = usePathname();
-    // const navbar = document.getElementById('navbar')
     const navbarRef = useRef()
+    const openButtonRef = useRef()
+
+
 
     function openSidebar(e) {
-        console.log("e", e);
+        // console.log("e", e);
 
         navbarRef.current.classList = 'show';
-        // openButton.setAttribute('aria-expanded', 'true')
-        // navbar.removeAttribute('inert')
+        openButtonRef.current.setAttribute('aria-expanded', 'true')
+        navbarRef.current.removeAttribute('inert')
     }
 
     function closeSidebar() {
         navbarRef.current.classList = '';
-        // openButton.setAttribute('aria-expanded', 'false')
-        // navbar.setAttribute('inert', '')
+        openButtonRef.current.setAttribute('aria-expanded', 'false')
+        navbarRef.current.setAttribute('inert', '')
     }
+
+    useEffect(() => {
+        const media = window.matchMedia("(width < 500px)")
+
+        function updateNavbar(e) {
+            const isMobile = e.matches
+            console.log("isMobile", isMobile);
+            
+            if (isMobile) {
+                navbarRef.current?.setAttribute('inert', '')
+            } else {
+                navbarRef.current?.removeAttribute('inert')
+            }
+        }
+
+        // Ejecutar al montar por si ya coincide
+        updateNavbar(media)
+
+        media.addEventListener('change', updateNavbar)
+
+        return () => {
+            media.removeEventListener('change', updateNavbar)
+        }
+    }, [])
+
 
     return (
         <>
             {/* Barra de navegación */}
             <header id='header' >
-                <button id='open-sidebar-button' onClick={openSidebar} >
+                <button ref={openButtonRef} id='open-sidebar-button' onClick={openSidebar} aria-label='open sidebar' aria-expanded="false" aria-controls='navbar'>
                     <FontAwesomeIcon icon={faBars} />
                 </button>
-                <nav ref={navbarRef}>
+                <nav ref={navbarRef} id='navbar'>
                     <ul>
                         <li>
-                            <button id='close-sidebar-button' onClick={closeSidebar}>
+                            <button id='close-sidebar-button' onClick={closeSidebar} aria-label='close sidebar'>
                                 <FontAwesomeIcon icon={faClose} />
                             </button>
                         </li>
-                        <li>
+                        <li aria-current="Página del terciario" >
                             <Link href="https://www.ifts18.edu.ar/home" target='#blank' >
                                 <Image src="/logo_ifts.jpg" alt="Logo terciario" width={40} height={40} />
                             </Link>
@@ -59,7 +87,9 @@ const Header = () => {
                         </li>
                     </ul>
                 </nav>
+                <div id="overlay" onClick={closeSidebar} aria-hidden="true" ></div>
             </header>
+
         </>
     )
 }
