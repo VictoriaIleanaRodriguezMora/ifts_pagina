799b4d9291ac0d6041432404743b6dbd4a11db12
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2026-04-01 19:27:40 -0300

    fix: imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true, // Evita problemas con imágenes en GitHub Pages
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: "export",
+  images: {
+    unoptimized: true, // Evita problemas con imágenes en GitHub Pages
+  },
+  sassOptions: {
+    includePaths: ['./src/app/styles'],
+  },
+};
+
+export default nextConfig;
